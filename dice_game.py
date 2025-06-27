b72a141fd3a79fa901707f2f8db320f186247ae6
hoshi haruka <haruka@wlan-napt-006.u-aizu.ac.jp>
2025-06-27 11:22:46 +0900

    Won/lost message added

--- dice_game.py
+++ dice_game.py
@@ -4,6 +4,12 @@ print("Rolling dice...")
 d1 = random.randint(1, 6)
 d2 = random.randint(1, 6)
 
+total = d1 + d2
 print(f"Die 1: {d1}")
 print(f"Die 2: {d2}")
-print(f"Total value: {d1 + d2}")
+print(f"Total value: {total}")
+
+if total > 7:
+    print("You won!")
+else:
+    print("You lost!")
